d9943025fc91ea027ef087675b29a2c136a1eeeb
kellerkichel <kellerkichel@hotmail.com>
2017-01-12 08:41:57 -0200

    added callback to funcs

--- dev/js/config/funcs/funcs.js
+++ dev/js/config/funcs/funcs.js
@@ -1,10 +1,12 @@
 ﻿(function () {
 
-    cfw.funcs.start = function () {
+    cfw.funcs.start = function (cb) {
         cfw.getJS(cfw.pathFile.plugin + 'funcs/funcs.min.js', function () {
 
             // evento funcs carregadas
             $(document).trigger('cfw_funcs_loaded');
+
+            if (typeof (cb) == 'function') cb();
 
         });
     }
