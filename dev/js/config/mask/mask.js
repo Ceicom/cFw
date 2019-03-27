8777eaf3a88b6228c14574748c5cde6fbb8baad8
Keller Renato Kichel <kellerkichel@hotmail.com>
2019-03-27 09:08:04 -0300

    globalizadas funções da mascara

--- dev/js/config/mask/mask.js
+++ dev/js/config/mask/mask.js
@@ -30,70 +30,71 @@
     }
 
     cfw.mask.dealMasks = function () {
-        function translateMasks($el) {
-
-            switch ($el.attr('data-mask')) {
-                case 'zip':
-                    setMaskAtribute($el, '00000-000');
-                    break;
-                case 'phone':
-                    maskPhoneDeal($el);
-                    break;
-                case 'date':
-                    setMaskAtribute($el, '00/00/0000');
-                    break;
-                case 'cnpj':
-                    setMaskAtribute($el, { 'data-mask': '00.000.000/0000-00', 'data-mask-reverse': true });
-                    break;
-                case 'cpf':
-                    setMaskAtribute($el, { 'data-mask': '000.000.000-00', 'data-mask-reverse': true });
-                    break;
-                case 'money':
-                    setMaskAtribute($el, { 'data-mask': '#.##0,00', 'data-mask-reverse': true });
-                    break;
-                case 'time':
-                    val = '00:00:00';
-                    break;
-            }
-        }
-
-        function maskPhoneDeal($el) {
-            $el.addClass('js-maskit-phone-mask').removeAttr('data-mask');
-
-            var w8 = setInterval(function () {
-                if (cfw.loadedJS[plugin] == 'loaded') {
-                    clearInterval(w8);
-
-                    var celphoneMask = ['(00) 00000-0000', '(00) 0000-00000'];
-                    $('.js-maskit-phone-mask').mask(celphoneMask[1], {
-                        onKeyPress:
-                           function (val, e, field, options) {
-                               field.mask(val.length > 14 ? celphoneMask[0] : celphoneMask[1], options);
-                           }
-                    });
-                }
-            }, 100);
-        }
-
-        function setMaskAtribute($el, atributes) {
-
-            var attrsMask = {};
-
-            if (typeof (atributes) == 'string')
-                attrsMask['data-mask'] = atributes;
-            else
-                attrsMask = atributes;
-
-            $.each(attrsMask, function (k, v) {
-                $el.attr(k, v);
-            });
-        }
-
         $('[data-mask]').each(function () {
-            translateMasks($(this));
+            cfw.mask.translateMasks($(this));
         });
 
         this.initiated = true;
+    }
+
+    cfw.mask.translateMasks = function($el) {
+
+        switch ($el.attr('data-mask')) {
+            case 'zip':
+                cfw.mask.setMaskAtribute($el, '00000-000');
+                break;
+            case 'phone':
+                cfw.mask.maskPhoneDeal($el);
+                break;
+            case 'date':
+                cfw.mask.setMaskAtribute($el, '00/00/0000');
+                break;
+            case 'cnpj':
+                cfw.mask.setMaskAtribute($el, { 'data-mask': '00.000.000/0000-00', 'data-mask-reverse': true });
+                break;
+            case 'cpf':
+                cfw.mask.setMaskAtribute($el, { 'data-mask': '000.000.000-00', 'data-mask-reverse': true });
+                break;
+            case 'money':
+                cfw.mask.setMaskAtribute($el, { 'data-mask': '#.##0,00', 'data-mask-reverse': true });
+                break;
+            case 'time':
+                val = '00:00:00';
+                break;
+        }
+    }
+
+    cfw.mask.setMaskAtribute = function($el, atributes) {
+
+        var attrsMask = {};
+
+        if (typeof (atributes) == 'string')
+            attrsMask['data-mask'] = atributes;
+        else
+            attrsMask = atributes;
+
+        $.each(attrsMask, function (k, v) {
+            $el.attr(k, v);
+        });
+    }
+
+    cfw.mask.maskPhoneDeal = function($el) {
+        $el.addClass('js-maskit-phone-mask').removeAttr('data-mask');
+
+        var w8 = setInterval(function () {
+            if (cfw.loadedJS[plugin] == 'loaded') {
+                clearInterval(w8);
+
+                var celphoneMask = ['(00) 00000-0000', '(00) 0000-00000'];
+
+                $('.js-maskit-phone-mask').mask(celphoneMask[1], {
+                    onKeyPress:
+                       function (val, e, field, options) {
+                           field.mask(val.length > 14 ? celphoneMask[0] : celphoneMask[1], options);
+                       }
+                });
+            }
+        }, 100);
     }
 
 })();
